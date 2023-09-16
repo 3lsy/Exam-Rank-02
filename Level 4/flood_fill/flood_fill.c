ce56e6de2bb56f70114cb2273af6577350f59f83
3lsy <107457733+3lsy@users.noreply.github.com>
2023-09-16 20:19:38 +0200

    flood_fill

--- Level 4/flood_fill/flood_fill.c
+++ Level 4/flood_fill/flood_fill.c
@@ -1,23 +1,18 @@
-typedef struct	s_point
-{
-	int			x;
-	int			y;
-}				t_point;
 
-void	fill(char **tab, t_point size, t_point cur, char to_fill)
+static void	fill(char **tab, t_point size, t_point p, char zone)
 {
-	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x
-		|| tab[cur.y][cur.x] != to_fill)
-		return;
-
-	tab[cur.y][cur.x] = 'F';
-	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
-	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
-	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
-	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+	if (0 <= p.x && p.x < size.x && 0 <= p.y && p.y < size.y
+		&& tab[p.y][p.x] == zone)
+	{
+		tab[p.y][p.x] = 'F';
+		fill(tab, size, (t_point){p.x - 1, p.y}, zone);
+		fill(tab, size, (t_point){p.x + 1, p.y}, zone);
+		fill(tab, size, (t_point){p.x, p.y - 1}, zone);
+		fill(tab, size, (t_point){p.x, p.y + 1}, zone);
+	}
 }
 
-void	flood_fill(char **tab, t_point size, t_point begin)
+void  flood_fill(char **tab, t_point size, t_point begin)
 {
 	fill(tab, size, begin, tab[begin.y][begin.x]);
 }

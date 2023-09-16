386c8cb5327c55c6337b00b6baf8a7cc1170fb2c
3lsy <107457733+3lsy@users.noreply.github.com>
2023-09-16 20:30:21 +0200

    fprime

--- Level 4/fprime/fprime.c
+++ Level 4/fprime/fprime.c
@@ -3,13 +3,10 @@
 
 int	main(int argc, char *argv[])
 {
-	int	i;
-	int	number;
-
 	if (argc == 2)
 	{
-		i = 1;
-		number = atoi(argv[1]);
+		int i = 1;
+		int number = atoi(argv[1]);
 		if (number == 1)
 			printf("1");
 		while (number >= ++i)
